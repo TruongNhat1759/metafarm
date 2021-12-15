fd6398dc2b474aca26a5721a94d36cf92e359c70
Ryan Truong <ryan@boostcommerce.net>
2021-12-15 08:49:26 +0700

    add timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,5 +67,21 @@ $(function(){
         $('.checkbox-custom-remember').toggleClass('active');
         $('#remember').attr('checked', !$('#remember').attr('checked'))
     })
-    
+    countDownTimer('Dec 23, 2021 00:00:00', '.seed-sale');
+    countDownTimer('Dec 26, 2021 18:25:05', '.private-sale');
 })
+function countDownTimer(timeEnd, section) {
+    const second = 1000, minute = second * 60, hour = minute * 60, day = hour * 24;
+	const countDown = new Date(timeEnd).getTime();
+	const timer = setInterval(function() {    
+		const now = new Date().getTime();
+		const distance = countDown - now;
+		$(section).find('.days').html(Math.floor(distance / (day)));
+		$(section).find('.hours').html(Math.floor((distance % (day)) / (hour)));
+		$(section).find('.minutes').html(Math.floor((distance % (hour)) / (minute)));
+		$(section).find('.seconds').html(Math.floor((distance % (minute)) / second));
+		if (distance < 1000) {
+			clearInterval(timer);
+		}
+	}, 1000)
+}
